fbe20fcb5a7ee2a165fa8f9996fcf8dcdd116c75
Renkinjutsu <lucas.cheng12@gmail.com>
2019-06-20 10:16:32 -0400

    next commit

--- index.js
+++
@@ -1,3 +0,0 @@
-import ReactDOM from 'react-dom'
-import React from 'react'
-import hi from "renkin-react";
--- renkin-react.jsx
+++ renkin-react.jsx
@@ -1,1 +1,9 @@
-module.export =  hi => "Present"
+import React from 'react'
+
+class BestAlarm extends React.Component {
+    render() {
+    return (
+      <button type="button">Turn on alarm</button>
+      )
+    }
+  }
